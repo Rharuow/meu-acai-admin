3eb176e5ed008db9d8c505724879b6b5047f96eb
Harysson Soares <haryssonsoares@gmail.com>
2023-02-25 12:14:25 -0300

    fix: lottie react lib

--- src/components/domain/home/index.tsx
+++ src/components/domain/home/index.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-import Switch from "react-switch";
+import React from "react";
 
 import { mockedOrders } from "@/src/entities/Order";
 import LastOrder from "./LastOrder";
 
 export default function HomePage() {
-  const [isOpen, setIsOpen] = useState(false);
-
   const waitingOrder = mockedOrders.find((order) => order.status === "making");
-
-  const handleStatus = () => {
-    setIsOpen((prevState) => !prevState);
-  };
 
   return (
     <div className="d-flex p-3 flex-wrap">
-      <LastOrder isOpen={isOpen} />
+      <LastOrder />
     </div>
   );
 }

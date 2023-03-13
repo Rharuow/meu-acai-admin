24c61af9a876a0028dcd6e2586b77d05af3f8b90
Harysson Soares <haryssonsoares@gmail.com>
2023-03-13 16:20:28 -0300

    refactor: order entite

--- src/entities/Order.ts
+++ src/entities/Order.ts
@@ -1,13 +1,23 @@
 import { Menu } from "./Product";
 import { User } from "./User";
 
+export type OrderStatus =
+  | "done"
+  | "canceled"
+  | "delivering"
+  | "making"
+  | "waiting";
+
 export type Order = {
   id?: string;
-  status: "done" | "canceled" | "delivering" | "making" | "waiting";
+  product_id: string;
+  user_id: string;
+  job_day_id: string;
+  status: OrderStatus;
   payment_method?: "PIX" | "Cartão" | "Espécie";
   payment_date?: string;
   created_at: string;
-  user?: User;
+  user: User;
   product: Menu;
 };
 
